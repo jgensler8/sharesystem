85f30cf61d9287d544790d91ab380c9644a8962b
Jeff Gensler <jgensler8@live.com>
2021-02-28 18:00:01 -0800

    add sub command scaffold to bootstrap tool

--- src/program-rust-bootstrap/src/main.rs
+++ src/program-rust-bootstrap/src/main.rs
@@ -11,8 +11,10 @@ use solana_client::{
     rpc_config::RpcSendTransactionConfig,
 };
 use std::convert::TryInto;
+use clap::{App, Arg};
 
-fn main() {
+
+fn bootstrap_search_engine_database() {
     // payer (read from file)
     let payer = read_keypair_file("./src/lib/keygen.out.json").unwrap();
     let payer_pubkey = payer.pubkey();
@@ -63,4 +65,59 @@ fn main() {
             ..RpcSendTransactionConfig::default()
         },
     );
+}
+
+fn bootstrap_resource_database() {
+
+}
+
+fn main() {
+    let matches = App::new("bootstrap")
+    .version("0.0.1")
+    .author("Jeff")
+    // payer
+    .arg(
+        Arg::new("payer-keypair-file")
+            .about("the file to read/save the private key to")
+            .default_value("./src/lib/keygen.out.json")
+    )
+    // search engine
+    .arg(
+        Arg::new("searchengine-pubkey-file")
+            .about("the file to read the public key from")
+            .default_value("./src/lib/searchengine_deploy_raw.out.json")
+    )
+    .arg(
+        Arg::new("searchengine-database-keypair-file")
+            .about("the file to read/save the private key to")
+            .default_value("./src/lib/searchengine_database_keygen.out.json")
+    )
+    // resource
+    .arg(
+        Arg::new("resource-pubkey-file")
+            .about("the file to read the public key from")
+            .default_value("./src/lib/resource_deploy_raw.out.json")
+    )
+    .arg(
+        Arg::new("resource-database-keypair-file")
+            .about("the file to read/save the private key to")
+            .default_value("./src/lib/resource_database_keygen.out.json")
+    )
+    // commands
+    .subcommand(
+        App::new("bootstrap-search-engine-database")
+            .about("Create a database account for a search engine to use")
+    )
+    .subcommand(
+        App::new("boostrap-resource-database")
+            .about("Create a database account for a resource to use")
+    )
+    .get_matches();
+
+    match matches.subcommand_name() {
+        Some("bootstrap-search-engine-database") => bootstrap_search_engine_database(),
+        Some("boostrap-resource-database") => bootstrap_resource_database(),
+        None => println!("No subcommand was used"),
+        _ => println!("Some other subcommand was used"),
+    }
 }
